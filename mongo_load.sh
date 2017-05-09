af9494e690da0f922947f78afe2a259f08ef202c
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-05-08 21:50:18 -0700

    docker-compose community edition and beta

--- mongo_load.sh
+++ mongo_load.sh
@@ -7,3 +7,8 @@ fi
 MONGO_HOST=${1:-$(docker-machine ip)}
 sed -ibak 's/"orgId" : "150e18f6-0165-34ad-ad30-8577b03eadb4/"orgId" : "39928fd4-b86a-36bf-8a06-20932b88ba81/g' load.js
 mongo $MONGO_HOST:27017 < load.js
+
+if [ -e load.js.private ]; then
+sed -ibak 's/"orgId" : "150e18f6-0165-34ad-ad30-8577b03eadb4/"orgId" : "39928fd4-b86a-36bf-8a06-20932b88ba81/g' load.js.private
+mongo $MONGO_HOST:27017 < load.js.private
+fi
