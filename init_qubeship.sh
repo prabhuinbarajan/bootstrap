01b25ff813b576abfb4e34eb9aa0f6d9fcda23e0
Hyunji Kim <hj.eunice@gmail.com>
2017-05-09 00:06:25 -0700

    files updated

--- init_qubeship.sh
+++ init_qubeship.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+# copy .client_env.template to .client_env
+if [ -e .client_env ]; then
+	echo 'qubeship is already pre-configured.'
+	exit 0
+fi
+
 QUBE_VAULT_SERVICE=qube-vault
 LOG_FILE=qube_vault_log
+
+# copy vault config.json and firebase.json to busybox
+docker-compose up -d busybox
+docker cp qubeship_home/vault/data/ "$(docker-compose ps -q busybox)":/vault/
 
 # start qube-vault service
 docker-compose up -d $QUBE_VAULT_SERVICE
@@ -15,12 +25,14 @@ VAULT_TOKEN=`cat $LOG_FILE | awk -F': ' 'NR==2{print $2}'`
 # unseal vault server
 $RUN_CMD unseal $UNSEAL_KEY
 
+cp .client_env.template .client_env
 # put key and token to .client_env
-sed -ibak "s/<unseal_key>/$UNSEAL_KEY/g" .client_env
+sed -ibak "s#<unseal_key>#$UNSEAL_KEY#g" .client_env
 sed -ibak "s/<vault_token>/$VAULT_TOKEN/g" .client_env
 
 # export variables in .client_env
-# TODO
+set -o allexport
+source .client_env
 
 # vault auth
 $RUN_CMD auth $VAULT_TOKEN
@@ -32,4 +44,7 @@ BASE_PATH="secret/resources"
 $RUN_CMD write $BASE_PATH/$TENANT/$ENV_TYPE/$ENV_ID/supertoken value=$VAULT_TOKEN
 $RUN_CMD write $BASE_PATH/$TENANT/$ENV_TYPE/$ENV_ID/usercreds_writer_token value=$VAULT_TOKEN
 # firebase
-$RUN_CMD write $BASE_PATH/$TENANT/$ENV_TYPE/$ENV_ID/firebase_qubeship api_key=$API_KEY service_key=@/vault/data/qubeship_firebase.json
+$RUN_CMD write $BASE_PATH/$TENANT/$ENV_TYPE/$ENV_ID/firebase_qubeship api_key=$FIREBASE_API_KEY service_key=@/vault/data/qubeship_firebase.json
+
+# install qubeship cli
+curl -sL http://cli.qubeship.io/install.sh | sh
