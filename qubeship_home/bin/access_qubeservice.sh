7bc6f10a24089d27da4e240387cb869c4bc8ed92
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-05-21 15:18:29 -0700

    service access

---
+++ qubeship_home/bin/access_qubeservice.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../..
+export PATH=$DIR:$PATH
+set +x -e
+
+PROJECT=$1
+CONTEXT_PATH=$(echo $2 | sed 's#/\(.*\)$#\1#')
+if [ -z $PROJECT ] ; then
+    echo "Usage: ${BASH_SOURCE[0]} qube_project_name context_path"
+    echo "Example: ${BASH_SOURCE[0]} myfirstjavaproject /api"
+    exit 1
+fi
+
+echo $PROJECT
+
+SERVICE=$PROJECT"-service"
+CLUSTER_IP=$($DIR/kubectl get services -o jsonpath='{.items[?(@.metadata.name=="qubefirstpythonproject-service")].spec.clusterIP}')
+
+echo "IP address of the $SERVICE : $CLUSTER_IP for $CONTEXT_PATH"
+echo "curl http://$CLUSTER_IP/$CONTEXT_PATH"
+
+curl http://$CLUSTER_IP/$CONTEXT_PATH
