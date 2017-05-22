6177b388c82f9d452e0917caea7663aa0371b73f
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-05-21 18:31:06 -0700

    minikube first time install fixes

--- provision_minikube.sh
+++ provision_minikube.sh
@@ -86,7 +86,7 @@ if [ "$default_namespace" != "default" ]; then
     echo "ERROR: default namespace not found. endpoint configuration may not be successful"
     exit 0
 fi
-
+set +e
 for i in `seq 1 3`;
 do
     DEFAULT_TOKEN_NAME=$(kubectl --namespace=kube-system get serviceaccount default -o jsonpath="{.secrets[0].name}")
@@ -98,7 +98,7 @@ do
         break
     fi
 done
-
+set -e
 if [  "$default_token" == "" ]; then
     echo "ERROR: default token not found. endpoint configuration may not be successful"
     exit 0
