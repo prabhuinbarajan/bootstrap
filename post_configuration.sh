8002220f289019015a0fc400e9e5df005dd5878d
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-05-30 14:21:19 -0700

    post configuraiton location

--- post_configuration.sh
+++ post_configuration.sh
@@ -132,10 +132,10 @@ echo "You can use your GITHUB credentials to login !!!!"
 if [ ! -z $BETA_ACCESS_USERNAME ];  then
     echo "APP: $APP_URL"
 fi
+qube service postconfiguration
 if [ "$install_sample_projects" == "true" ] ; then
     pguid=$(uuidgen)
     jguid=$(uuidgen)
-    qube service postconfiguration
     echo "creating python sample project"
     qube service create --service-name "QubeFirstPythonProject" --repo-name "$pguid" --language=python
     echo "creating java sample project"
