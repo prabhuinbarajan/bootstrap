b7399daf93b2b179c92c2af2dac9213e450db73c
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2017-05-30 10:20:02 -0700

    changing permissions on docker socket for different environments

--- pull_latest.sh
+++ pull_latest.sh
@@ -2,15 +2,12 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 set -o allexport
-source $DIR/qube_common_functions.sh
-
+files="-f docker-compose.yaml"
 if [ -f $BETA_CONFIG_FILE ]; then
     echo "sourcing $BETA_CONFIG_FILE"
     source $BETA_CONFIG_FILE
-fi
-
-if [ $is_beta ];  then
     docker login -u $BETA_ACCESS_USERNAME -p $BETA_ACCESS_TOKEN quay.io
+    files="$files -f docker-compose-beta.yaml"
 fi
 
 docker-compose $files pull
